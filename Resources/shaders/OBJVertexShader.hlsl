ffbaa35f4ae0aa6018dc5a7c69784b1fea9a0ac4
YamanakaRui <k021g1123@g.neec.ac.jp>
2022-12-12 06:10:27 +0900

    02

--- Resources/shaders/OBJVertexShader.hlsl
+++ Resources/shaders/OBJVertexShader.hlsl
@@ -2,9 +2,18 @@
 
 VSOutput main(float4 pos : POSITION, float3 normal : NORMAL, float2 uv : TEXCOORD)
 {
-	VSOutput output; // ピクセルシェーダーに渡す値
+	// 右下奥　向きのライト
+	float3 lightdir = float3(1, -1, 1);
+	lightdir = normalize(lightdir);
+	// ライト色
+	float3 lightcolor = float3(1, 1, 1);
+	// ピクセルシェーダーに渡す値
+	VSOutput output;
 	output.svpos = mul(mat, pos);
-	output.normal = normal;
+	// Lambert
+	output.color.rgb = dot(-lightdir, normal) * m_diffuse * lightcolor;
+	output.color.a = m_alpha;
 	output.uv = uv;
+
 	return output;
 }
