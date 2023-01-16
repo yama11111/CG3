10b370cf001db2507be53d63c546d3b1fab2cf80
YamanakaRui <k021g1123@g.neec.ac.jp>
2023-01-17 02:56:57 +0900

    05_03まで

--- Resources/shaders/OBJPixelShader.hlsl
+++ Resources/shaders/OBJPixelShader.hlsl
@@ -18,6 +18,26 @@ float4 main(VSOutput input) : SV_TARGET
 {
 	// テクスチャマッピング
 	float4 texcolor = tex.Sample(smp, input.uv);
+
+	float4 shadercolor;
+	// 光沢度
+	const float shininess = 4.0f;
+	// 頂点から視点への方向ベクトル
+	float3 eyedir = normalize(cameraPos - input.worldpos.xyz);
+	
+	float3 dotlightnormal = dot(lightv, input.normal);
+	// 反射光ベクトル
+	float3 reflect = normalize(-lightv + 2 * dotlightnormal * input.normal);
+	// 環境反射光
+	float3 ambient = m_ambient;
+	// 拡散反射光
+	float3 diffuse = dotlightnormal * m_diffuse;
+	// 鏡面反射光
+	float3 specular = pow(saturate(dot(reflect, eyedir)), shininess) * m_specular;
+
+	shadercolor.rgb = (ambient + diffuse + specular) * lightcolor;
+	shadercolor.a = m_alpha;
+
 	// シェーディング色で描画
-	return input.color * texcolor;
+	return shadercolor * texcolor;
 }
--- Resources/shaders/OBJShaderHeader.hlsli
+++ Resources/shaders/OBJShaderHeader.hlsli
@@ -24,7 +24,8 @@ cbuffer cbuff2 : register(b2)
 struct VSOutput
 {
 	float4 svpos : SV_POSITION; // システム用頂点座標
-	//float3 normal :NORMAL; // 法線ベクトル
-	float4 color :COLOR; // 法線ベクトル
+	//float4 color :COLOR;
+	float4 worldpos :POSITION;
+	float3 normal :NORMAL; // 法線ベクトル
 	float2 uv  :TEXCOORD; // uv値
 };
